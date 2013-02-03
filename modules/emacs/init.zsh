d61612f7f709094cc3aba4b097dbb69014e99d4c
Sebastian Wiesner <lunaryorn@gmail.com>
2013-02-03 10:15:17 +0100

    Add Emacs module
    Enables Emacs dependency management with Carton.

--- modules/emacs/init.zsh
+++ modules/emacs/init.zsh
@@ -4,22 +4,12 @@
 # Authors: Sebastian Wiesner <lunaryorn@gmail.com>
 #
 
-# Return if requirements are not found.
-if [[ ! -d "$HOME/.cask" ]]; then
-  return 1
+# Enable Carton
+if [[ -d "$HOME/.carton" ]]; then
+    path=($HOME/.carton/bin $path)
+
+    alias cai='carton install'
+    alias cau='carton update'
+    alias caI='carton init'
+    alias cae='carton exec'
 fi
-
-# Prepend Cask bin directory.
-path=($HOME/.cask/bin $path)
-
-# Load Carton completion
-source "$HOME/.cask/etc/cask_completion.zsh" 2> /dev/null
-
-#
-# Aliases
-#
-
-alias cai='cask install'
-alias cau='cask update'
-alias caI='cask init'
-alias cae='cask exec'

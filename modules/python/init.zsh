7cf2b8e6270ab9c83a9ccc59325b8e7db61d1b68
Adam Walz <adam@adamwalz.net>
2014-05-05 09:39:24 -0700

    Use Anaconda python installation if it is available

--- modules/python/init.zsh
+++ modules/python/init.zsh
@@ -15,6 +15,10 @@ if [[ -s "$HOME/.pyenv/bin/pyenv" ]]; then
 elif (( $+commands[pyenv] )); then
   eval "$(pyenv init -)"
 
+# anaconda python installer
+elif [[ -d "$HOME/.anaconda/bin" ]]; then
+  path=($HOME/.anaconda/bin $path)
+
 # Prepend PEP 370 per user site packages directory, which defaults to
 # ~/Library/Python on Mac OS X and ~/.local elsewhere, to PATH. The
 # path can be overridden using PYTHONUSERBASE.

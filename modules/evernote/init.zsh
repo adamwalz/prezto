e73810c20e614e054bf4744edfa257ce41b4601b
Adam Walz <adam@adamwalz.net>
2014-07-30 15:06:01 -0700

    Update Evernote module to work with the web service project

--- modules/evernote/init.zsh
+++ modules/evernote/init.zsh
@@ -5,11 +5,15 @@
 #   Adam Walz <adam@adamwalz.net>
 #
 
-export PROJECT_HOME="$HOME/Developer/Evernote"
-export MAC_HOME="$PROJECT_HOME/mac"
-
-export JAVA_HOME=`/usr/libexec/java_home -v 1.6`
+export EVERNOTE_PROJ_ROOT="$HOME/Developer/Evernote"
+export MAC_HOME="$EVERNOTE_PROJ_ROOT/mac"
+export WEB_HOME="$EVERNOTE_PROJ_ROOT/web"
+export NOTESTORE_HOME="$EVERNOTE_PROJ_ROOT/notestore"
 export ANDROID_HOME="/usr/local/opt/android-sdk"
+
+export JAVA_HOME=`/usr/libexec/java_home -v 1.7`
+export TOMCAT_HOME="/usr/local/Cellar/tomcat6/6.0.41/libexec"
+
 
 # Add Android tools to path
 if [[ -s "$ANDROID_HOME/tools/android" ]]; then
@@ -22,8 +26,8 @@ if [[ -s "$ANDROID_HOME/platform-tools/adb" ]]; then
 fi
 
 # Add DevTools repository commands to path
-if [[ -s "$PROJECT_HOME/DevTools/bin/enreview" ]]; then
-  path=("$PROJECT_HOME/DevTools/bin" $path)
+if [[ -s "$EVERNOTE_PROJ_ROOT/DevTools/bin/enreview" ]]; then
+  path=("$EVERNOTE_PROJ_ROOT/DevTools/bin" $path)
 fi
 
 #
@@ -31,14 +35,43 @@ fi
 #
 
 function enmacfix {
-  integer build=$((`./build-scripts/build-number.sh | tail -n 1` + 1))
-  echo "Fixed in nightly build $build" | tee >(pbcopy)
+  if [[ $PWD =~ "$MAC_HOME(/.*)?" ]]; then
+    build=$((`$MAC_HOME/build-scripts/build-number.sh -n | tail -1`))
+    echo "Fixed in nightly build $build" | tr -d '\n' | tee >(pbcopy)
+  else
+    echo "First cd to $MAC_HOME"
+  fi
+}
+
+function enmacstage {
+  stageon=`defaults read com.evernote.Evernote stage`
+  if [[ $stageon == "YES" ]]; then
+    defaults write com.evernote.Evernote stage NO
+    echo "stage off"
+  else
+    defaults write com.evernote.Evernote stage YES
+    echo "stage on"
+  fi
 }
 
 #
 # Aliases
 #
 
-alias init_project='python $PROJECT_HOME/DevTools/init_project.py'
-alias enstageon='defaults write com.evernote.Evernote stage YES'
-alias enstageoff='defaults write com.evernote.Evernote stage NO'
+alias init_project='/usr/bin/python $EVERNOTE_PROJ_ROOT/DevTools/init_project.py'
+alias enweblog="tail -F $TOMCAT_HOME/logs/catalina.out"
+
+# Compile a module without running the interminable test cases:
+alias mvnnotest="mvn install -Dmaven.test.skip=true"
+
+# Install a built version of notestore
+alias cpnstore="cp $HOME/.m2/repository/com/evernote/notestore/0.1-SNAPSHOT/notestore-0.1-SNAPSHOT.jar\
+                   $TOMCAT_HOME/webapps/ROOT/WEB-INF/lib/"
+
+# Grab any new schema changes
+alias enblast="$NOTESTORE_HOME/src/main/sql/blast-sql.sh"
+
+# To connect to the databases
+alias ustore="mysql -u root userstore"
+alias nstore="mysql -u root notestore"
+

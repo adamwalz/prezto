0069cc760f5e1284d275c6f45cee65a23ac2bb3a
Adam Walz <adam@adamwalz.net>
2015-08-05 15:20:51 -0700

    Use docker-machine instead of boot2docker

--- modules/docker/init.zsh
+++ modules/docker/init.zsh
@@ -5,14 +5,9 @@
 #   Adam Walz <adam@adamwalz.net>
 #
 
-# Load manually installed boot2docker environment into the shell session.
-if [[ "$OSTYPE" == darwin* ]] && (( $+commands[boot2docker] )); then
-  if [[ $(boot2docker status) == "running" ]]; then
-    export DOCKER_CERT_PATH=$HOME/.boot2docker/certs/boot2docker-vm
-    export DOCKER_TLS_VERIFY=1
-    export DOCKER_HOST=tcp://192.168.59.103:2376
-  fi
+docker_machine_name="dev"
 
-  alias dockerup='boot2docker up && eval $(boot2docker shellinit)'
-  alias dockerdown='boot2docker down'
+# Load manually installed boot2docker environment into the shell session.
+if (( $+commands[docker-machine] )); then
+  eval "$(docker-machine env ${docker_machine_name})"
 fi

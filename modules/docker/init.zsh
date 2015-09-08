aef9e8a43876415f4781a2055b0b19a39831dd4b
Adam Walz <adam@adamwalz.net>
2015-09-08 13:52:02 -0600

    Don't load docker-compose environment in Tmux

--- modules/docker/init.zsh
+++ modules/docker/init.zsh
@@ -9,5 +9,7 @@ docker_machine_name="dev"
 
 # Load manually installed boot2docker environment into the shell session.
 if (( $+commands[docker-machine] )); then
-  eval "$(docker-machine env ${docker_machine_name})"
+  # Don't load the docker-machine environment when running in tmux because it
+  # slows down creating new windows
+  [[ -z $TMUX ]] && eval "$(docker-machine env ${docker_machine_name})"
 fi

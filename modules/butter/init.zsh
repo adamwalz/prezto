613f49401e259458c27e35d32cd9cebb4eba01b5
Adam Walz <adam@adamwalz.net>
2017-10-16 10:50:47 -0700

    Add kops alias' to Butter module

--- modules/butter/init.zsh
+++ modules/butter/init.zsh
@@ -9,6 +9,12 @@
 # Aliases
 #
 
+export KOPS_STATE_STORE=s3://clusters.ops.butter.ai
+
 alias kcs='kubectl --context=stage'
 alias kcp='kubectl --context=prod'
 alias kco='kubectl --context=ops'
+
+alias bdev='kops export kubecfg butterdevtest.ops.butter.ai'
+alias bstage='kops export kubecfg butterstage.ops.butter.ai'
+alias bprod='kops export kubecfg butter.ai'

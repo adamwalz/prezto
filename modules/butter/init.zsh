7bc0ae101a0663e923d67bdc8251d76eea74aef0
Adam Walz <adam@adamwalz.net>
2019-12-24 13:08:54 -0700

    Set kubectl context without rewriting entire kube config file

--- modules/butter/init.zsh
+++ modules/butter/init.zsh
@@ -11,10 +11,8 @@
 
 export KOPS_STATE_STORE=s3://clusters.ops.butter.ai
 
-alias kcs='kubectl --context=stage'
-alias kcp='kubectl --context=prod'
-alias kco='kubectl --context=ops'
-
-alias bdev='kops export kubecfg butterdevtest.ops.butter.ai'
-alias bstage='kops export kubecfg butterstage.ops.butter.ai'
-alias bprod='kops export kubecfg butter.ai'
+alias kcp="sed -i -e 's/current-context:.*/current-context: production/' ~/.kube/config"
+alias kcs="sed -i -e 's/current-context:.*/current-context: stage/' ~/.kube/config"
+alias kcd="sed -i -e 's/current-context:.*/current-context: devtest/' ~/.kube/config"
+alias kcm="sed -i -e 's/current-context:.*/current-context: minikube/' ~/.kube/config"
+alias kcw="sed -i -e 's/current-context:.*/current-context: walznet/' ~/.kube/config"
